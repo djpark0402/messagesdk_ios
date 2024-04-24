bffd01ae42edaec0d4e1c83260d9e8f4417f79bc
djpark0402 <djpark0402@raoncorp.com>
2024-04-24 09:57:39 +0900

    build: init project
    swift framework project

---
+++ MessageSDK/MessageSDK/MessageSDK.h
@@ -0,0 +1,18 @@
+//
+//  MessageSDK.h
+//  MessageSDK
+//
+//  Created by dong jun park on 4/24/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MessageSDK.
+FOUNDATION_EXPORT double MessageSDKVersionNumber;
+
+//! Project version string for MessageSDK.
+FOUNDATION_EXPORT const unsigned char MessageSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessageSDK/PublicHeader.h>
+
+
